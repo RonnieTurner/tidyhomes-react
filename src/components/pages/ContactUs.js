7eafb8f6cf30bcbec9e00006594c65e428613ef6
Ronnie Turner <ronnie.turner.303@gmail.com>
2021-10-06 17:55:19 -0600

    Create ContactUs.js

---
+++ src/components/pages/ContactUs.js
@@ -0,0 +1,12 @@
+import ContactUsForm from "../components/forms/ContactUsForm";
+
+function ContactUs() {
+  return (
+    <section>
+      <h1>Contact Us</h1>
+      <ContactUsForm />
+    </section>
+  );
+}
+
+export default ContactUs;
